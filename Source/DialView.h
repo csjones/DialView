d7fb3953a35487f0e1bcdec3f6c26200b2c6f98c
Tyler Bell <tyler@tylerwbell.com>
2015-07-04 10:58:04 -0400

    initial

---
+++ Source/DialView.h
@@ -0,0 +1,19 @@
+//
+//  DialView.h
+//  DialView
+//
+//  Created by twb on 7/4/15.
+//  Copyright © 2015 twb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DialView.
+FOUNDATION_EXPORT double DialViewVersionNumber;
+
+//! Project version string for DialView.
+FOUNDATION_EXPORT const unsigned char DialViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DialView/PublicHeader.h>
+
+
